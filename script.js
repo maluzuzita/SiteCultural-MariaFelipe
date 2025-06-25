20a0c173f67950d63d2c3f17bd460040de6b0587
maluu <lyymariaaa@gmail.com>
2025-06-25 07:52:15 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+// <-- Funções para as abas -->
+function abaHistoria() {
+    window.location.href = "historia.html";
+} // Aba da página sobre a história do cordel
+
+function abaArteLiteratura() {
+    window.location.href = "arte.html";
+}  // Aba da página que contém a história do processo da arte e literatura do cordel
+
+function abaGaleria() {
+   window.location.href = "galeria.html";
+}  // Aba da página que contém a galeria de imagens, vídeos, obras e muito mais sobre o cordel
